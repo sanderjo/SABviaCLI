791b33912c66f3d2684ca06727ef97317d0f7bcf
Sander Jo <sanderjonkers+github@gmail.com>
2019-05-03 21:17:17 +0200

    try SABnzbd's INI, otherwise SABnzbd's webinterface

--- sab_show_queue_and_history.sh
+++ sab_show_queue_and_history.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-#APIKEY="8cf72df3a90ac9652a4d58a48f028352"
+# Get Queue and History from the local SABnzbd
 
-#APIKEY=`grep ^api_key ~/.sabnzbd/sabnzbd.ini | awk '{ print$NF }'`
+# Try to get APIKEY:
+# ... from the ini file
 APIKEY=$(awk '/^api_key/ { print $NF }'  ~/.sabnzbd/sabnzbd.ini)
-
-echo "API Key found:" $APIKEY
+if [ -n "$APIKEY" ]; then
+  echo "API Key found via ini file:" $APIKEY
+else
+  # ... or else via the webinterface
+  echo "Trying via webinterface"
+  #APIKEY=$(lynx --dump 'http://127.0.0.1:8080/sabnzbd/config/general/' | grep API | awk '{ print $3 }')
+  APIKEY=$(wget -o /dev/null  -O- 'http://127.0.0.1:8080/sabnzbd/config/general/' |  awk -F\' '/sabSession/ { print $2 }')
+  if [ -n "$APIKEY" ]; then
+    echo "API Key found via webinterface" $APIKEY
+  else
+    echo "no API Key found!!!"
+    exit
+  fi
+fi
+# APIKEY should now be something like "8cf72df3a90ac9652a4d58a48f028352"
 
 #lynx --dump 'http://127.0.0.1:8080/sabnzbd/api?output=json&apikey=8cf72df3a90ac9652a4d58a48f028352&mode=history' | python3 -m json.tool  | grep -B1 nzb_name | grep '"name"'
-
 #lynx --dump 'http://127.0.0.1:8080/sabnzbd/api?output=json&apikey=$APIKEY&mode=history' | python3 -m json.tool  | grep -B1 nzb_name | grep '"name"'
-
-#URL='http://127.0.0.1:8080/sabnzbd/api?output=json&apikey=8cf72df3a90ac9652a4d58a48f028352&mode=history'
-
-#URL1='http://127.0.0.1:8080/sabnzbd/api?output=json&apikey=8cf72df3a90ac9652a4d58a48f028352'
-#URL2='&mode=history'
-
 
 BASEURL='http://127.0.0.1:8080/sabnzbd/api?output=json&apikey='
 BASEURL+="$APIKEY"
